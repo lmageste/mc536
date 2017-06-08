b363d23be053a0314a8693138152c44af924011c
Miorim <rmmiorim@hotmail.com>
2017-06-07 23:30:10 -0300

    Create script_tratamento_trata_doenca.cpp

--- script_tratamento_trata_doenca.cpp
+++ script_tratamento_trata_doenca.cpp
@@ -9,18 +9,24 @@ using namespace std;
 
 int main () {
     int n;
+    int m;
     scanf(" %d ", &n);
+    scanf(" %d ", &m);
     vector<string> doenca = vector<string> (n, "");
+    vector<string> tratamento = vector<string> (m,"");
     for (int i = 0; i < n; ++i){
         getline(cin, doenca[i]);
     }
+    for(int i = 0; i < m; ++i){
+        getline(cin, tratamento[i]);
+    }
     
-    int m = 8329;
+    
     
     for(int i=0; i<n; i++){
         for (int j=0; j<m; j++) {
             if(rand()%8000==0){
-                cout << "INSERT INTO tratamento_trata_doenca (doenca_nome_cientifico, tratamento_id) VALUE " << "(\"" << doenca[i] << "\", \"" <<j << "\");" << endl;
+                cout << "INSERT INTO tratamento_trata_doenca (doenca_nome_cientifico, nome_tratamento) VALUE " << "(\"" << doenca[i] << "\", \"" << tratamento[j] << "\");" << endl;
             }
         }
     }
